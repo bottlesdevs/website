f39169e5a4ecd8c5b3d8771310a953ae2304bdf2
Radu Avramescu <contact@raduavramescu.com>
2022-06-11 13:07:34 +0300

    Refactored donations.js

--- assets/js/donations.js
+++ assets/js/donations.js
@@ -3,21 +3,26 @@ class Donations {
         var gpeople = list.github;
         var gother = list.other;
         var html = "";
-        html += "<h4>GitHub</h4>";
-        for (var i = 0; i < gpeople.length; i++) {
-            html += `
-            <div class="person">
-                <img src="https://avatars.githubusercontent.com/${gpeople[i]}?s=120" alt="${gpeople[i]}">
-                <span>${gpeople[i]}</span>
+
+        addSection('GitHub', gpeople, true);
+        addSection('Other platforms', gother);
+
+        function addSection(sectionTitle, personList, hasPic = false) {
+            html += `<h4>${sectionTitle}</h4>`;
+            const personClasses = hasPic ? 'person' : 'person no-pic';
+
+            personList.forEach(person => {
+                html += getPersonMarkup(person, personClasses, hasPic);
+            });
+        }
+
+        function getPersonMarkup(person, personClasses, hasPic) {
+            return `<div class="${personClasses}">` +
+            (hasPic ? `<img src="https://avatars.githubusercontent.com/${person}?s=120" alt="${person}">` : ``) +
+            `<span>${person}</span>
             </div>`;
         }
-        html += "<h4>Other platforms</h4>";
-        for (var i = 0; i < gother.length; i++) {
-            html += `
-            <div class="person no-pic">
-                <span>${gother[i]}</span>
-            </div>`;
-        }
+
         document.querySelector(dom).innerHTML = html;
     }
 }
