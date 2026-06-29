ea29c6dd3b2eceba4bbe9ee7cf88d5b2695fd099
mirkobrombin <brombin94@gmail.com>
2026-06-29 10:43:56 +0200

    fix: arab lang switch

--- i18n/translations/index.js
+++ i18n/translations/index.js
@@ -1,11 +1,13 @@
 import { en } from './en';
 import { it } from './it';
 import { es } from './es';
+import { ar } from './ar';
 
-export { en, it, es };
+export { en, it, es, ar };
 
 export const translations = {
     en,
     it,
     es,
+    ar,
 };
